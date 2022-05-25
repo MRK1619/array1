eb97299fa0f9a0503ebfaa7365325e438e31c025
Partha Sarathi Hazra <sarathiparthahazra9609@gmail.com>
2022-05-25 23:00:03 +0530

    initial commit

---
+++ array2.py
@@ -0,0 +1,9 @@
+#Python program to reverse an array
+def reverseList(A):
+    print( A[::-1])
+	
+# Driver function to test above function
+A = [45, 39, 290, 48, 9, 23,484 , 93]
+print(A)
+print("Reversed list is")
+reverseList(A)
