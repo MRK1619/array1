397595279fc8d420aa74a36adb61cb835bfdcd2b
Partha Sarathi Hazra <sarathiparthahazra9609@gmail.com>
2022-05-26 23:02:09 +0530

    Find An Element

---
+++ array5.py
@@ -0,0 +1,28 @@
+#Search An Pivot Element In A Given Arrey 
+def search (arr, l, h, key):
+	if l > h:
+		return -1
+	
+	mid = (l + h) // 2
+	if arr[mid] == key:
+		return mid
+
+	# If arr[l...mid] Is Sorted
+	if arr[l] <= arr[mid]:
+		if key >= arr[l] and key <= arr[mid]:
+			return search(arr, l, mid-1, key)
+		return search(arr, mid + 1, h, key)
+    # If arr[mid+1...h] Is Sorted
+	if key >= arr[mid] and key <= arr[h]:
+		return search(arr, mid + 1, h, key)
+	return search(arr, l, mid-1, key)
+
+# Driver program
+arr = [4, 5, 6, 7, 8, 9, 1, 2, 3]
+key = 9
+i = search(arr, 0, len(arr)-1, key)
+if i != -1:
+	print ("Index: % d"% i)
+else:
+	print ("Key not found")
+
